a8f347cadadc3994c7ae5b993a0f3b448448ff54
puko313 <pa.mendoza319@gmail.com>
2023-09-18 04:18:19 +0900

    with supabase.js

--- with-tailwindcss-app/src/app/page.tsx
+++ with-tailwindcss-app/src/app/page.tsx
@@ -1,11 +1,25 @@
-import Image from 'next/image'
+import { supabase } from "../../utils/supabase"
 
-export default function Home() {
+export default function Home({lessons}) {
+  console.log(lessons);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <h1>Main</h1>
+        {lessons.map(lesson => {
+          <p>{lessons.title}</p>
+        })}
       </div>
     </main>
   )
 }
+
+export const getStaticProps = async () => {
+  const { data: lessons } = await supabase.from('lesson').select('*');
+
+  return {
+    props: {
+      lessons,
+    },
+  }
+}
+
